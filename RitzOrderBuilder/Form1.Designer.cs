eb2c1acd3e01ff3c58dd6b2c2bc4b4ad9efbdb0c
ritzitsdev <jcampbell@ritzcamera.com>
2014-07-02 11:17:12 -0400

    Populate Store List

---
+++ RitzOrderBuilder/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace RitzOrderBuilder
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.lblDownloading = new System.Windows.Forms.Label();
+      this.selStoreNumber = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // backgroundWorker1
+      // 
+      this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      // 
+      // lblDownloading
+      // 
+      this.lblDownloading.AutoSize = true;
+      this.lblDownloading.Location = new System.Drawing.Point(2, 26);
+      this.lblDownloading.Name = "lblDownloading";
+      this.lblDownloading.Size = new System.Drawing.Size(119, 13);
+      this.lblDownloading.TabIndex = 0;
+      this.lblDownloading.Text = "Updating. Please wait...";
+      // 
+      // selStoreNumber
+      // 
+      this.selStoreNumber.Enabled = false;
+      this.selStoreNumber.FormattingEnabled = true;
+      this.selStoreNumber.Location = new System.Drawing.Point(5, 72);
+      this.selStoreNumber.Name = "selStoreNumber";
+      this.selStoreNumber.Size = new System.Drawing.Size(121, 21);
+      this.selStoreNumber.TabIndex = 1;
+      this.selStoreNumber.Text = "Select Store";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(397, 376);
+      this.Controls.Add(this.selStoreNumber);
+      this.Controls.Add(this.lblDownloading);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    private System.Windows.Forms.Label lblDownloading;
+    private System.Windows.Forms.ComboBox selStoreNumber;
+  }
+}
+
---
+++ RitzOrderBuilder/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RitzOrderBuilder
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      backgroundWorker1.RunWorkerAsync();
+    }
+
+    private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+    {
+      //Get the latest version of orderbuilder.xml
+      string urlLocations = "http://ritzpix.com/media/files/orderbuilder.xml";
+      string pathLocations = @"C:\APM\orderbuilder.xml";
+
+      Uri url = new Uri(urlLocations);
+      HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+      HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+      response.Close();
+      Int64 iSize = response.ContentLength;
+      Int64 iRunningByteTotal = 0;
+
+      using (WebClient client = new WebClient())
+      {
+        using (Stream streamRemote = client.OpenRead(new Uri(urlLocations)))
+        {
+          using (Stream streamLocal = new FileStream(pathLocations, FileMode.Create, FileAccess.Write, FileShare.None))
+          {
+            int iByteSize = 0;
+            byte[] byteBuffer = new byte[iSize];
+            while ((iByteSize = streamRemote.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
+            {
+              streamLocal.Write(byteBuffer, 0, iByteSize);
+              iRunningByteTotal += iByteSize;
+
+              double dIndex = (double)(iRunningByteTotal);
+              double dTotal = (double)byteBuffer.Length;
+              double dProgressPercentage = (dIndex / dTotal);
+              int iProgressPercentage = (int)(dProgressPercentage * 100);
+
+              backgroundWorker1.ReportProgress(iProgressPercentage);
+            }
+            streamLocal.Close();
+          }
+          streamRemote.Close();
+        }
+      }
+    } //end backgroundWorker1_DoWork
+
+    private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      this.lblDownloading.Text = (e.ProgressPercentage.ToString() + "%");
+    } //end backgroundWorker1_ProgressChanged
+
+    private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      if (!(e.Error == null))
+      {
+        this.lblDownloading.Text = "Could not download store information.  " + e.Error.Message;
+      }
+      else
+      {
+        lblDownloading.Visible = false;
+        populateStoreList();
+      }
+    } //end backgroundWorker1_RunWorkerCompleted
+
+    private void populateStoreList()
+    {
+      //populate store number combobox
+      string pathLocations = @"C:\Program Files\ITS\OrderBuilder\buildersettings.xml";
+      if (File.Exists(pathLocations))
+      {
+        XDocument xDocLocations = XDocument.Load(pathLocations);
+        var qryLocations = from locations in xDocLocations.Descendants("location")
+                           select new { Store = locations.Attribute("store_number").Value };
+        selStoreNumber.ValueMember = "Store";
+        selStoreNumber.DisplayMember = "Store";
+        selStoreNumber.DataSource = qryLocations.ToList();
+        selStoreNumber.BindingContext = new BindingContext();
+
+      }
+    }
+  }
+}
