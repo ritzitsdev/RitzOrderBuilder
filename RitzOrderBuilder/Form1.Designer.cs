d9dcbeace8dc3d2474614942c26307ef36c260ec
ritzitsdev <jcampbell@ritzcamera.com>
2014-07-05 13:58:46 -0400

    dialogs

--- RitzOrderBuilder/Form1.Designer.cs
+++ RitzOrderBuilder/Form1.Designer.cs
@@ -31,15 +31,38 @@
       this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
       this.lblDownloading = new System.Windows.Forms.Label();
       this.selStoreNumber = new System.Windows.Forms.ComboBox();
+      this.lblCustInfo = new System.Windows.Forms.Label();
+      this.custFirstName = new System.Windows.Forms.TextBox();
+      this.lblFirstName = new System.Windows.Forms.Label();
+      this.custLastName = new System.Windows.Forms.TextBox();
+      this.lblLastName = new System.Windows.Forms.Label();
+      this.custPhone = new System.Windows.Forms.TextBox();
+      this.lblPhone = new System.Windows.Forms.Label();
+      this.custEmail = new System.Windows.Forms.TextBox();
+      this.lblEmail = new System.Windows.Forms.Label();
+      this.lblSelectProduct = new System.Windows.Forms.Label();
+      this.productList = new System.Windows.Forms.ComboBox();
+      this.lblSelectBookPDF = new System.Windows.Forms.Label();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.button1 = new System.Windows.Forms.Button();
+      this.PDFLocation = new System.Windows.Forms.TextBox();
+      this.lblCoverPath = new System.Windows.Forms.Label();
+      this.jpgLocation = new System.Windows.Forms.TextBox();
+      this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+      this.button2 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.quantity = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
       // 
       // backgroundWorker1
       // 
       this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
       // 
       // lblDownloading
       // 
       this.lblDownloading.AutoSize = true;
+      this.lblDownloading.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
       this.lblDownloading.Location = new System.Drawing.Point(2, 26);
       this.lblDownloading.Name = "lblDownloading";
       this.lblDownloading.Size = new System.Drawing.Size(119, 13);
@@ -50,20 +73,214 @@
       // 
       this.selStoreNumber.Enabled = false;
       this.selStoreNumber.FormattingEnabled = true;
-      this.selStoreNumber.Location = new System.Drawing.Point(5, 72);
+      this.selStoreNumber.Location = new System.Drawing.Point(16, 46);
       this.selStoreNumber.Name = "selStoreNumber";
-      this.selStoreNumber.Size = new System.Drawing.Size(121, 21);
+      this.selStoreNumber.Size = new System.Drawing.Size(101, 21);
       this.selStoreNumber.TabIndex = 1;
       this.selStoreNumber.Text = "Select Store";
+      // 
+      // lblCustInfo
+      // 
+      this.lblCustInfo.AutoSize = true;
+      this.lblCustInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblCustInfo.Location = new System.Drawing.Point(5, 83);
+      this.lblCustInfo.Name = "lblCustInfo";
+      this.lblCustInfo.Size = new System.Drawing.Size(126, 13);
+      this.lblCustInfo.TabIndex = 2;
+      this.lblCustInfo.Text = "Customer Information";
+      // 
+      // custFirstName
+      // 
+      this.custFirstName.Location = new System.Drawing.Point(17, 119);
+      this.custFirstName.Name = "custFirstName";
+      this.custFirstName.Size = new System.Drawing.Size(97, 20);
+      this.custFirstName.TabIndex = 3;
+      // 
+      // lblFirstName
+      // 
+      this.lblFirstName.AutoSize = true;
+      this.lblFirstName.Location = new System.Drawing.Point(18, 104);
+      this.lblFirstName.Name = "lblFirstName";
+      this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+      this.lblFirstName.TabIndex = 4;
+      this.lblFirstName.Text = "First Name";
+      // 
+      // custLastName
+      // 
+      this.custLastName.Location = new System.Drawing.Point(127, 119);
+      this.custLastName.Name = "custLastName";
+      this.custLastName.Size = new System.Drawing.Size(100, 20);
+      this.custLastName.TabIndex = 5;
+      // 
+      // lblLastName
+      // 
+      this.lblLastName.AutoSize = true;
+      this.lblLastName.Location = new System.Drawing.Point(128, 104);
+      this.lblLastName.Name = "lblLastName";
+      this.lblLastName.Size = new System.Drawing.Size(58, 13);
+      this.lblLastName.TabIndex = 6;
+      this.lblLastName.Text = "Last Name";
+      // 
+      // custPhone
+      // 
+      this.custPhone.Location = new System.Drawing.Point(243, 119);
+      this.custPhone.Name = "custPhone";
+      this.custPhone.Size = new System.Drawing.Size(91, 20);
+      this.custPhone.TabIndex = 7;
+      // 
+      // lblPhone
+      // 
+      this.lblPhone.AutoSize = true;
+      this.lblPhone.Location = new System.Drawing.Point(244, 104);
+      this.lblPhone.Name = "lblPhone";
+      this.lblPhone.Size = new System.Drawing.Size(38, 13);
+      this.lblPhone.TabIndex = 8;
+      this.lblPhone.Text = "Phone";
+      // 
+      // custEmail
+      // 
+      this.custEmail.Location = new System.Drawing.Point(16, 160);
+      this.custEmail.Name = "custEmail";
+      this.custEmail.Size = new System.Drawing.Size(210, 20);
+      this.custEmail.TabIndex = 9;
+      // 
+      // lblEmail
+      // 
+      this.lblEmail.AutoSize = true;
+      this.lblEmail.Location = new System.Drawing.Point(16, 145);
+      this.lblEmail.Name = "lblEmail";
+      this.lblEmail.Size = new System.Drawing.Size(32, 13);
+      this.lblEmail.TabIndex = 10;
+      this.lblEmail.Text = "Email";
+      // 
+      // lblSelectProduct
+      // 
+      this.lblSelectProduct.AutoSize = true;
+      this.lblSelectProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblSelectProduct.Location = new System.Drawing.Point(8, 302);
+      this.lblSelectProduct.Name = "lblSelectProduct";
+      this.lblSelectProduct.Size = new System.Drawing.Size(91, 13);
+      this.lblSelectProduct.TabIndex = 11;
+      this.lblSelectProduct.Text = "Select Product";
+      // 
+      // productList
+      // 
+      this.productList.FormattingEnabled = true;
+      this.productList.Location = new System.Drawing.Point(17, 319);
+      this.productList.Name = "productList";
+      this.productList.Size = new System.Drawing.Size(299, 21);
+      this.productList.TabIndex = 12;
+      this.productList.Text = "Select Product";
+      // 
+      // lblSelectBookPDF
+      // 
+      this.lblSelectBookPDF.AutoSize = true;
+      this.lblSelectBookPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblSelectBookPDF.Location = new System.Drawing.Point(10, 197);
+      this.lblSelectBookPDF.Name = "lblSelectBookPDF";
+      this.lblSelectBookPDF.Size = new System.Drawing.Size(173, 13);
+      this.lblSelectBookPDF.TabIndex = 13;
+      this.lblSelectBookPDF.Text = "Location of PDF for the Book";
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.Filter = "PDF Files (*.pdf)|*.pdf";
+      this.openFileDialog1.InitialDirectory = "%SytemDrive%\\%USERPROFILE%\\desktop";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(322, 212);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 14;
+      this.button1.Text = "Browse";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // PDFLocation
+      // 
+      this.PDFLocation.Location = new System.Drawing.Point(17, 213);
+      this.PDFLocation.Name = "PDFLocation";
+      this.PDFLocation.Size = new System.Drawing.Size(299, 20);
+      this.PDFLocation.TabIndex = 15;
+      // 
+      // lblCoverPath
+      // 
+      this.lblCoverPath.AutoSize = true;
+      this.lblCoverPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblCoverPath.Location = new System.Drawing.Point(11, 249);
+      this.lblCoverPath.Name = "lblCoverPath";
+      this.lblCoverPath.Size = new System.Drawing.Size(209, 13);
+      this.lblCoverPath.TabIndex = 16;
+      this.lblCoverPath.Text = "Location of JPG for the Book Cover";
+      // 
+      // jpgLocation
+      // 
+      this.jpgLocation.Location = new System.Drawing.Point(17, 266);
+      this.jpgLocation.Name = "jpgLocation";
+      this.jpgLocation.Size = new System.Drawing.Size(299, 20);
+      this.jpgLocation.TabIndex = 17;
+      // 
+      // openFileDialog2
+      // 
+      this.openFileDialog2.Filter = "JPG Files (*.jpg)|*.jpg";
+      this.openFileDialog2.InitialDirectory = "%USERPROFILE%\\desktop";
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(322, 264);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 18;
+      this.button2.Text = "Browse";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(322, 302);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(54, 13);
+      this.label1.TabIndex = 19;
+      this.label1.Text = "Quantity";
+      // 
+      // quantity
+      // 
+      this.quantity.Location = new System.Drawing.Point(322, 319);
+      this.quantity.Name = "quantity";
+      this.quantity.Size = new System.Drawing.Size(75, 20);
+      this.quantity.TabIndex = 20;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(397, 376);
+      this.ClientSize = new System.Drawing.Size(417, 444);
+      this.Controls.Add(this.quantity);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.jpgLocation);
+      this.Controls.Add(this.lblCoverPath);
+      this.Controls.Add(this.PDFLocation);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.lblSelectBookPDF);
+      this.Controls.Add(this.productList);
+      this.Controls.Add(this.lblSelectProduct);
+      this.Controls.Add(this.lblEmail);
+      this.Controls.Add(this.custEmail);
+      this.Controls.Add(this.lblPhone);
+      this.Controls.Add(this.custPhone);
+      this.Controls.Add(this.lblLastName);
+      this.Controls.Add(this.custLastName);
+      this.Controls.Add(this.lblFirstName);
+      this.Controls.Add(this.custFirstName);
+      this.Controls.Add(this.lblCustInfo);
       this.Controls.Add(this.selStoreNumber);
       this.Controls.Add(this.lblDownloading);
       this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
@@ -76,6 +293,27 @@
     private System.ComponentModel.BackgroundWorker backgroundWorker1;
     private System.Windows.Forms.Label lblDownloading;
     private System.Windows.Forms.ComboBox selStoreNumber;
+    private System.Windows.Forms.Label lblCustInfo;
+    private System.Windows.Forms.TextBox custFirstName;
+    private System.Windows.Forms.Label lblFirstName;
+    private System.Windows.Forms.TextBox custLastName;
+    private System.Windows.Forms.Label lblLastName;
+    private System.Windows.Forms.TextBox custPhone;
+    private System.Windows.Forms.Label lblPhone;
+    private System.Windows.Forms.TextBox custEmail;
+    private System.Windows.Forms.Label lblEmail;
+    private System.Windows.Forms.Label lblSelectProduct;
+    private System.Windows.Forms.ComboBox productList;
+    private System.Windows.Forms.Label lblSelectBookPDF;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.TextBox PDFLocation;
+    private System.Windows.Forms.Label lblCoverPath;
+    private System.Windows.Forms.TextBox jpgLocation;
+    private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox quantity;
   }
 }
 
--- RitzOrderBuilder/Form1.cs
+++ RitzOrderBuilder/Form1.cs
@@ -28,77 +28,80 @@ namespace RitzOrderBuilder
 
     private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
     {
-      //Get the latest version of orderbuilder.xml
-      string urlLocations = "http://ritzpix.com/media/files/orderbuilder.xml";
-      string pathLocations = @"C:\APM\orderbuilder.xml";
+      //Get the latest version of ritzsetup.xml (for store locations) and builderSettings.xml
+      string remoteStoreInfoXml = "http://ritzpix.com/media/files/ritzsetup.xml";
+      string localStoreInfoXml = @"C:\Program Files\ITS\OrderBuilder\ritzsetup.xml";
+      string remoteBuilderSettings = "http://ritzpix.com/media/files/builderSettings.xml";
+      string localBuilderSettings = @"C:\Program Files\ITS\OrderBuilder\builderSettings.xml";
 
-      Uri url = new Uri(urlLocations);
-      HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-      HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-      response.Close();
-      Int64 iSize = response.ContentLength;
-      Int64 iRunningByteTotal = 0;
-
-      using (WebClient client = new WebClient())
-      {
-        using (Stream streamRemote = client.OpenRead(new Uri(urlLocations)))
-        {
-          using (Stream streamLocal = new FileStream(pathLocations, FileMode.Create, FileAccess.Write, FileShare.None))
-          {
-            int iByteSize = 0;
-            byte[] byteBuffer = new byte[iSize];
-            while ((iByteSize = streamRemote.Read(byteBuffer, 0, byteBuffer.Length)) > 0)
-            {
-              streamLocal.Write(byteBuffer, 0, iByteSize);
-              iRunningByteTotal += iByteSize;
-
-              double dIndex = (double)(iRunningByteTotal);
-              double dTotal = (double)byteBuffer.Length;
-              double dProgressPercentage = (dIndex / dTotal);
-              int iProgressPercentage = (int)(dProgressPercentage * 100);
-
-              backgroundWorker1.ReportProgress(iProgressPercentage);
-            }
-            streamLocal.Close();
-          }
-          streamRemote.Close();
-        }
-      }
+      WebClient client = new WebClient();
+      client.DownloadFile(remoteStoreInfoXml, localStoreInfoXml);
+      client.DownloadFile(remoteBuilderSettings, localBuilderSettings);
     } //end backgroundWorker1_DoWork
-
-    private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
-    {
-      this.lblDownloading.Text = (e.ProgressPercentage.ToString() + "%");
-    } //end backgroundWorker1_ProgressChanged
 
     private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
     {
       if (!(e.Error == null))
       {
-        this.lblDownloading.Text = "Could not download store information.  " + e.Error.Message;
+        this.lblDownloading.Text = "Could not download updates. " + e.Error.Message;
       }
       else
       {
-        lblDownloading.Visible = false;
-        populateStoreList();
+        lblDownloading.Text = "Select Store";
+        lblDownloading.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        selStoreNumber.Enabled = true;
       }
+        populateStoreList();
+        populateProductList();
     } //end backgroundWorker1_RunWorkerCompleted
 
     private void populateStoreList()
     {
       //populate store number combobox
-      string pathLocations = @"C:\Program Files\ITS\OrderBuilder\buildersettings.xml";
+      string pathLocations = @"C:\Program Files\ITS\OrderBuilder\ritzsetup.xml";
       if (File.Exists(pathLocations))
       {
         XDocument xDocLocations = XDocument.Load(pathLocations);
         var qryLocations = from locations in xDocLocations.Descendants("location")
                            select new { Store = locations.Attribute("store_number").Value };
+
         selStoreNumber.ValueMember = "Store";
         selStoreNumber.DisplayMember = "Store";
         selStoreNumber.DataSource = qryLocations.ToList();
         selStoreNumber.BindingContext = new BindingContext();
-
       }
+    } //end populateStoreList
+
+    private void populateProductList()
+    {
+      //populate Product List combobox
+      string pathProducts = @"C:\Program Files\ITS\OrderBuilder\builderSettings.xml";
+      if (File.Exists(pathProducts))
+      {
+        XDocument xDocProducts = XDocument.Load(pathProducts);
+        var qryProducts = from products in xDocProducts.Descendants("product")
+                          select new { ProductID = products.Attribute("id").Value, productName = products.Element("name").Value };
+
+        productList.ValueMember = "ProductID";
+        productList.DisplayMember = "productName";
+        productList.DataSource = qryProducts.ToList();
+        productList.BindingContext = new BindingContext();
+      }
+    } //end populateProductList
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      DialogResult result = openFileDialog1.ShowDialog();
+      string file = openFileDialog1.FileName;
+      PDFLocation.Text = file;
     }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      DialogResult result = openFileDialog2.ShowDialog();
+      string file = openFileDialog2.FileName;
+      jpgLocation.Text = file;
+    } //end populateProductList
+
   }
 }
